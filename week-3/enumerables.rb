e46932f1741823918c3c52ff2e7b965d8e4d2170
Charles Ellis <hamled@hamled.name>
2018-02-21 17:50:25 -0800

    Week 3 - Commit 1

--- week-3/enumerables.rb
+++ week-3/enumerables.rb
@@ -1,5 +1,20 @@
 # Cover enumerables topics:
 # - map
+
+# Map is many-to-many enumerable method
+
+snowboards = [
+  { name: "Ultra Snowboard 5", color: "white", size: :M, price: 50.00 },
+  { name: "Waverunner", color: "red", size: :L, price: 75.00 }
+]
+
+snowboard_colors = snowboards.map do |s|
+  s[:color]
+end
+
+puts "Snowboard colors: "
+puts snowboard_colors
+
 # - reduce
-# - filter
+# - filter / select
 # - all? / any?
