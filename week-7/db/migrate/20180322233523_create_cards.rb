f36ff7648b69ffc2fae6980cdc53ba7c597a55ac
Charles Ellis <hamled@hamled.name>
2018-03-22 16:37:43 -0700

    Card model

---
+++ week-7/db/migrate/20180322233523_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.string :job
+      t.string :location
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
