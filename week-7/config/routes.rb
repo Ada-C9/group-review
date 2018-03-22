52b904faa124139ddb5ce5413d855659b15a0dab
Charles Ellis <hamled@hamled.name>
2018-03-22 16:50:06 -0700

    Route to controller action

--- week-7/config/routes.rb
+++ week-7/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # HTTP method/verb | path     |    controller#action
+  get                 '/cards',  to: 'cards#index'
+
+  # Also valid:
+  #get                '/cards',  controller: 'cards', action: :index
 end
