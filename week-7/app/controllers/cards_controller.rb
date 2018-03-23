631798df8e996f0efc09e45a9fac3de99aecae18
Charles Ellis <hamled@hamled.name>
2018-03-22 18:58:05 -0700

    Output full card details

--- week-7/app/controllers/cards_controller.rb
+++ week-7/app/controllers/cards_controller.rb
@@ -1,5 +1,24 @@
 class CardsController < ApplicationController
   def index
-    render({ plain: "Business Cards!"})
+    card = Card.new({
+      name: "Ada Lovelace",
+      job: "Countess",
+      location: "England",
+      details: "The first programmer!",
+      acquired_on: "1815-12-10"
+    })
+
+    output = <<~HEREDOC
+    Business Cards:
+      #{card.name}
+      #{card.job}
+
+      #{card.location}
+      #{card.details}
+
+      #{card.acquired_on.to_s(:medium)}
+    HEREDOC
+
+    render({ plain: output})
   end
 end
