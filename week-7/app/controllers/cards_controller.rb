6b1ad53039b12ce9aeedc9603745eee3eda474f5
Charles Ellis <hamled@hamled.name>
2018-03-22 19:13:30 -0700

    Business card HTML in ERB view template

--- week-7/app/controllers/cards_controller.rb
+++ week-7/app/controllers/cards_controller.rb
@@ -8,19 +8,6 @@ class CardsController < ApplicationController
       acquired_on: "1815-12-10"
     })
 
-    output = <<~HEREDOC
-    <h1>Business Cards:</h1>
-    <div class="card">
-      <div class="salutation">
-        <h2 class="name"><%= @card.name %></h2>
-        <h3 class="job"><%= @card.job %></h3>
-      </div>
-      <h4 class="location"><%= @card.location %></h4>
-      <p class="details"><%= @card.details %></p>
-      <h4 class="acquired-on"><%= time_ago_in_words(@card.acquired_on) %></h4>
-    </div>
-    HEREDOC
-
-    render({ inline: output})
+    render("index")
   end
 end
