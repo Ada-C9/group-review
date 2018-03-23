c31db97f9431a150b3b959f35ba8cc1986479967
Charles Ellis <hamled@hamled.name>
2018-03-22 17:04:22 -0700

    Render with ERB view template

--- week-7/app/controllers/cards_controller.rb
+++ week-7/app/controllers/cards_controller.rb
@@ -7,20 +7,6 @@ class CardsController < ApplicationController
       description: "The first programmer!"
     })
 
-    some_erb = <<~ERB
-    <h1>Business Cards:</h1>
-    <ul class="cards">
-      <li class="card">
-        <div class="salutation">
-          <h1 class="name"><%= @card.name %></h1>
-          <h2 class="job"><%= @card.job %></h2>
-        </div>
-        <h3 class="location"><%= @card.location %></h3>
-        <p class="description"><%= @card.description %></p>
-      </li>
-    </ul>
-    ERB
-
-    render inline: some_erb
+    render "index"
   end
 end
