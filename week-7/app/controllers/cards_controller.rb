c8526b1723f60cfc4f10eb78a63ed8af23775dd5
Charles Ellis <hamled@hamled.name>
2018-03-22 19:08:36 -0700

    Render HTML business card

--- week-7/app/controllers/cards_controller.rb
+++ week-7/app/controllers/cards_controller.rb
@@ -1,6 +1,6 @@
 class CardsController < ApplicationController
   def index
-    card = Card.new({
+    @card = Card.new({
       name: "Ada Lovelace",
       job: "Countess",
       location: "England",
@@ -9,16 +9,18 @@ class CardsController < ApplicationController
     })
 
     output = <<~HEREDOC
-    Business Cards:
-      #{card.name}
-      #{card.job}
-
-      #{card.location}
-      #{card.details}
-
-      #{card.acquired_on.to_s(:medium)}
+    <h1>Business Cards:</h1>
+    <div class="card">
+      <div class="salutation">
+        <h2 class="name"><%= @card.name %></h2>
+        <h3 class="job"><%= @card.job %></h3>
+      </div>
+      <h4 class="location"><%= @card.location %></h4>
+      <p class="details"><%= @card.details %></p>
+      <h4 class="acquired-on"><%= time_ago_in_words(@card.acquired_on) %></h4>
+    </div>
     HEREDOC
 
-    render({ plain: output})
+    render({ inline: output})
   end
 end
