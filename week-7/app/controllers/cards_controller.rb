4a560fe046df2039b9e21f37004d8405bf26ee91
Charles Ellis <hamled@hamled.name>
2018-03-22 17:02:37 -0700

    Render with inline ERB template

--- week-7/app/controllers/cards_controller.rb
+++ week-7/app/controllers/cards_controller.rb
@@ -1,21 +1,26 @@
 class CardsController < ApplicationController
   def index
-    card = Card.new({
+    @card = Card.new({
       name: "Ada Lovelace",
       job: "Countess",
       location: "England",
       description: "The first programmer!"
     })
 
-    output = <<~HEREDOC
-    Business Cards:
-      #{card.name}
-      #{card.job}
+    some_erb = <<~ERB
+    <h1>Business Cards:</h1>
+    <ul class="cards">
+      <li class="card">
+        <div class="salutation">
+          <h1 class="name"><%= @card.name %></h1>
+          <h2 class="job"><%= @card.job %></h2>
+        </div>
+        <h3 class="location"><%= @card.location %></h3>
+        <p class="description"><%= @card.description %></p>
+      </li>
+    </ul>
+    ERB
 
-      #{card.location}
-      #{card.description}
-    HEREDOC
-
-    render plain: output
+    render inline: some_erb
   end
 end
