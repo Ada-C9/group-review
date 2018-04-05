c73b9db6f70b2e39be5f63939c6198ea1318ebe8
Charles Ellis <hamled@hamled.name>
2018-04-04 20:15:06 -0700

    Use resources shorthand in routes config

--- week-9/config/routes.rb
+++ week-9/config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # 3. to:
   # 4. controller#action
 
-  get('/cards', {to: 'cards#index', as: 'cards'})
+  # get('/cards', {to: 'cards#index', as: 'cards'})
+  #
+  # get('/cards/new', {to: 'cards#new', as: 'new_card'})
+  # get('/cards/:id', {to: 'cards#show', as: 'card'})
 
-  get('/cards/new', {to: 'cards#new', as: 'new_card'})
-  get('/cards/:id', {to: 'cards#show', as: 'card'})
+  resources(:cards, {:only => [:index, :new, :show]})
+
 
   #get('/cards', {controller: 'cards', action: 'index'})
 end
