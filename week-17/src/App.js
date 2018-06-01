43f7fcb3c7b1a4b3e6ff430b8dfe6990cfb5f1ac
Charles Ellis <hamled@hamled.name>
2018-05-31 17:41:05 -0700

    Week 17: Data in App component

--- week-17/src/App.js
+++ week-17/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // Our shopping list contains:
+    // Poptarts, 2 boxes
+    // ---> { name: 'Poptarts', quantity: 2, unit: 'boxes' }
+    // Clementines, 1 bag
+    // Chocolate Milk, 3 cartons
+
+    this.state = {
+      shoppingList: [
+        { name: 'Poptarts', quantity: 2, unit: 'boxes' },
+        { name: 'Clementines', quantity: 1, unit: 'bag' },
+        { name: 'Chocolate Milk', quantity: 3, unit: 'cartons' }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
